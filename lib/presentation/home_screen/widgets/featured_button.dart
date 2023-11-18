90fad1d8094997178e7f81315d5a3dfaa76e6a81
Navid Rahman <navid@hellokompass.com>
2023-11-18 13:31:04 +0600

    Home UI added

---
+++ lib/presentation/home_screen/widgets/featured_button.dart
@@ -0,0 +1,26 @@
+import 'package:daraz_idea_firebase/constants/consts.dart';
+
+Widget featuredButton({
+  String? title,
+  icon,
+}) {
+  return Row(
+    children: [
+      Image.asset(
+        icon,
+        width: 60,
+        fit: BoxFit.fill,
+      ),
+      10.widthBox,
+      title!.text.fontFamily(semibold).color(darkFontGrey).make(),
+    ],
+  )
+      .box
+      .white
+      .width(200)
+      .margin(const EdgeInsets.symmetric(horizontal: 4))
+      .padding(const EdgeInsets.all(4))
+      .roundedSM
+      .outerShadowSm
+      .make();
+}
